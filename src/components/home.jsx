fe6b1ba6a5e4958abf0b17fdab3dbbc3b70c3ab7
David Athobe <dathobek@gmail.com>
2018-07-19 16:57:19 +0100

    styling-home-page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,20 @@ export default class Home extends Component {
             <div style={{width:'100%',margin:'auto'}}>
                <Grid className="home-grid">
                    <Cell col={12}>
+                     <img 
+                       src="./assets/david1.jpg"
+                       alt="image"
+                       className="my-img" 
+                    />
+                    <div className="banner-text">
+                      <h1>Full Stack Developer</h1>
 
+                      <hr/>
+
+                      <p>HTML5/CSS3 | Bootstrap | JavaScript | React | Angular6 | JavaSE | MySql | Oracle 11g</p>
+                    </div>
+
+                   
                    </Cell>
                </Grid>
                  
