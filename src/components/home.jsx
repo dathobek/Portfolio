cd9b76f2284bcf83c221aa89448e844e1b208c21
David Athobe <dathobek@gmail.com>
2018-07-19 03:39:44 +0100

    some-styling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import {Grid,Cell} from 'react-mdl';
 
 
 export default class Home extends Component {
     render(){
         return(
-            <div>
-                 <h1>Home Page</h1>
+            <div style={{width:'100%',margin:'auto'}}>
+               <Grid className="home-grid">
+                   <Cell col={12}>
+
+                   </Cell>
+               </Grid>
+                 
             </div>
         )
     }
